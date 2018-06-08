6f23cca9b536081e003827223b284f6083963119
r-yanyo <honma.ryosuke@gmail.com>
2018-06-08 18:45:55 +0900

    create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+3.times do |i|
+  Post.create!(title: 'Sample Post'+i.to_s(), content: 'Sample Content')
+end

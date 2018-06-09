61980e75dd5c093709e016d3cb51155d476bbfbf
r-yanyo <honma.ryosuke@gmail.com>
2018-06-09 14:33:26 +0900

    use element-ui for header

--- app/javascript/Header.vue
+++ app/javascript/Header.vue
@@ -1,7 +1,24 @@
 <template>
   <div>
-    <router-link to="/">TOP</router-link>
-    <router-link to="about">ABOUT</router-link>
-    <router-link to="new">新規投稿</router-link>
+    <el-menu :default-active="$route.fullPath" :router="true" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+      <el-menu-item index="/">TOP</el-menu-item>
+      <el-menu-item index="about">ABOUT</el-menu-item>
+      <el-menu-item index="new">新規投稿</el-menu-item>
+    </el-menu>
   </div>
 </template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      activeIndex: "1"
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(this.activeIndex);
+    }
+  }
+};
+</script>

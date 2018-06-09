0ec4caa72d412db8149fd48a06936c0231ad442c
r-yanyo <honma.ryosuke@gmail.com>
2018-06-09 16:16:41 +0900

    fix active menu bug

--- app/javascript/Header.vue
+++ app/javascript/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-menu :default-active="$route.fullPath" :router="true" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+    <el-menu :default-active="activeIndex" :router="true" class="el-menu-demo" mode="horizontal" @select="handleSelect">
       <el-menu-item index="/">TOP</el-menu-item>
       <el-menu-item index="about">ABOUT</el-menu-item>
       <el-menu-item index="new">新規投稿</el-menu-item>
@@ -12,13 +12,14 @@
 export default {
   data: function() {
     return {
-      activeIndex: "1"
+      activeIndex: ""
     };
   },
+  mounted: function() {
+    this.activeIndex = this.$route.fullPath.slice(1);
+  },
   methods: {
-    handleSelect(key, keyPath) {
-      console.log(this.activeIndex);
-    }
+    handleSelect(key, keyPath) {}
   }
 };
 </script>

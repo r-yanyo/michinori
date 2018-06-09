83dda35f8dc861809bf6de2d21ec583de57129ea
r-yanyo <honma.ryosuke@gmail.com>
2018-06-09 16:06:59 +0900

    import ja lang library

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -8,9 +8,10 @@
 import Vue from "vue";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
+import locale from "element-ui/lib/locale/lang/ja";
 import router from "../router.js";
 
-Vue.use(ElementUI);
+Vue.use(ElementUI, { locale });
 
 import App from "../app.vue";
 

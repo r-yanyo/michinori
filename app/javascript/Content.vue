a0f178a045799c9600fb4103c3771a6bb38c7aa8
r-yanyo <honma.ryosuke@gmail.com>
2018-06-09 17:25:37 +0900

    implement paging

--- app/javascript/Content.vue
+++ app/javascript/Content.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
+  <div class="content">
     <router-view></router-view>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.content {
+  min-height: 100%;
+}
+</style>
--- app/javascript/Index.vue
+++ app/javascript/Index.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
   <ul>
     <li v-for="post in posts" :key="post.id">
       <el-card class="box-card">
@@ -11,6 +12,11 @@
       </el-card>
     </li>
   </ul>
+  <el-pagination
+    layout="prev, pager, next"
+    :total="50" :current-page.sync="currentPage">
+  </el-pagination>
+</div>
 </template>
 
 <script>
@@ -19,15 +25,21 @@ import axios from "axios";
 export default {
   data: function() {
     return {
-      posts: []
+      posts: [],
+      currentPage: 1
     };
   },
   mounted: function() {
     this.fetchPosts();
   },
+  watch: {
+    currentPage: function(bef, af) {
+      this.fetchPosts(this.currentPage);
+    }
+  },
   methods: {
-    fetchPosts: function() {
-      axios.get("/api/posts").then(
+    fetchPosts: function(pageNum) {
+      axios.get(`/api/posts?page=${pageNum}`).then(
         res => {
           this.posts = res.data.posts;
         },
@@ -41,16 +53,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+ul {
+  padding: 0;
+}
 li {
   list-style: none;
 }
 .box-card {
+  margin-bottom: 10px;
   .header h2 {
     margin: 5px;
   }
   .item p {
     line-height: 40px;
   }
+}
+.v-enter-active {
+  transition: opacity 1s;
+}
+.v-enter {
+  opacity: 0;
+}
+.v-enter-to {
+  opacity: 1;
 }
 </style>
 

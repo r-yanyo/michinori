f940c4584bd3fdebf3b8194b48fdd220b87eb3fb
r-yanyo <honma.ryosuke@gmail.com>
2018-06-10 15:08:14 +0900

    change element-ui font

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -25,6 +25,10 @@ p {
 </style>
 
 <style lang="scss">
+body {
+  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB",
+    "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+}
 p {
   line-height: 1.65;
 }

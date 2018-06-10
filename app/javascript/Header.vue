6cd43c62cbbf2c5d0c683771bad5f383ce50f46b
r-yanyo <honma.ryosuke@gmail.com>
2018-06-10 14:13:05 +0900

    sync activeIndex with url

--- app/javascript/Header.vue
+++ app/javascript/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-menu :default-active="activeIndex" :router="true" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+    <el-menu :default-active="activeIndex" :router="true" class="el-menu-demo" mode="horizontal">
       <el-menu-item index="/">TOP</el-menu-item>
       <el-menu-item index="/about">ABOUT</el-menu-item>
       <el-menu-item index="/new">新規投稿</el-menu-item>
@@ -11,15 +11,12 @@
 <script>
 export default {
   data: function() {
-    return {
-      activeIndex: ""
-    };
+    return {};
   },
-  mounted: function() {
-    this.activeIndex = this.$route.fullPath;
-  },
-  methods: {
-    handleSelect(key, keyPath) {}
+  computed: {
+    activeIndex: function() {
+      return this.$route.fullPath;
+    }
   }
 };
 </script>

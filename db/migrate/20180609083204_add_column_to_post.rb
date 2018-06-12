2f50a7016020b7021643d89675f11f3ddbb18cf4
r-yanyo <honma.ryosuke@gmail.com>
2018-06-12 20:23:45 +0900

    Add user model

--- db/migrate/20180609083204_add_column_to_post.rb
+++ db/migrate/20180609083204_add_column_to_post.rb
@@ -1,5 +1,6 @@
 class AddColumnToPost < ActiveRecord::Migration[5.1]
   def change
     add_column :posts, :likeNum, :integer
+    add_column :posts, :user_id, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180609085054) do
+ActiveRecord::Schema.define(version: 20180612111752) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20180609085054) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "likeNum", default: 0
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", limit: 191, null: false
+    t.string "email", limit: 191, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

0e70d44e6319d7eb6953929ee16698d58ff6b7f3
r-yanyo <honma.ryosuke@gmail.com>
2018-06-13 00:44:06 +0900

    add bcrypt gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :current_user
-
-  def current_user
-    remember_token = User.encrypt(cookies[:user_remember_token])
-    @current_user ||= User.find_by(remember_token: remember_token)
-  end
 
   def sign_in(user)
     remember_token = User.new_remember_token
@@ -16,5 +10,5 @@ class ApplicationController < ActionController::Base
   def sign_out
     @current_user = nil
   end
-  
+
 end

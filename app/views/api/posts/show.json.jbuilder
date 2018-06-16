b868697c227fbbeeb3c108d5fef3c60ad230a4eb
r-yanyo <honma.ryosuke@gmail.com>
2018-06-16 15:37:26 +0900

    remove user_id in send post

--- app/views/api/posts/show.json.jbuilder
+++ app/views/api/posts/show.json.jbuilder
@@ -1,3 +1,3 @@
 json.set! :post do
-  json.extract! @post, :id, :title, :content, :likeNum, :user_id, :author, :created_at, :updated_at
+  json.extract! @post, :id, :title, :content, :likeNum, :author, :created_at, :updated_at
 end
--- db/migrate/20180609085054_change_column_to_post.rb
+++ db/migrate/20180609085054_change_column_to_post.rb
@@ -2,5 +2,6 @@ class ChangeColumnToPost < ActiveRecord::Migration[5.1]
   def change
     change_column :posts, :likeNum, :integer, default: 0
     change_column :posts, :author, :string, default: "名無しさん"
+    change_column :posts, :user_id, :integer, default: nil
   end
 end

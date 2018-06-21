1125168aefdb3107a0702dda24c5db4271fc69ed
r-yanyo <honma.ryosuke@gmail.com>
2018-06-22 00:32:40 +0900

    centering paragraph

--- app/javascript/About.vue
+++ app/javascript/About.vue
@@ -1,15 +1,10 @@
 <template>
-<div>
-  <el-row>
-    <el-col :span="24">
-    <div class="grid-content bg-purple-dark main-logo">
-      <h1>道のり</h1>
-      </div>
-    </el-col>
-  </el-row>
+<div class="container">
+  <div class="grid-content bg-purple-dark main-logo">
+    <h1>道のり</h1>
+  </div>
   <div>
-    <p>あなたが今まで「努力」した経験をシェアしませんか？</p>
-    <p>「道のり」はあなたが努力した経験を次の世代へ引き継ぎます。</p>
+    <p>あなたが今まで「努力」した経験をシェアしませんか？「道のり」はあなたが努力した経験を次の世代へ引き継ぎます。</p>
     <p>注意: このサービスは<b>ベータ版</b>です。投稿内容は承諾無しに削除される可能性があります。</p>
   </div>
 </div>
@@ -19,6 +14,11 @@
 @font-face {
   font-family: "KouzanSoushoOTF";
   src: url("./KouzanSoushoOTF.otf");
+}
+.container {
+  max-width: 700px;
+  margin: 0 auto;
+  text-align: center;
 }
 .main-logo {
   padding: 40px;

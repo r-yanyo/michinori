3d4b25c824eaa1a62d96c91c5487343c38417525
r-yanyo <honma.ryosuke@gmail.com>
2018-06-22 23:43:56 +0900

    move adming pass

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -1,2 +1,2 @@
 # create administor
-@user = User.create!(name: "admin", email: "honma.ryosuke@gmail.com", password: "foobar", remember_token: User.encrypt(User.new_remember_token))
+@user = User.create!(name: "admin", email: "honma.ryosuke@gmail.com", password: ENV["ADMIN_PASS"], remember_token: User.encrypt(User.new_remember_token))
